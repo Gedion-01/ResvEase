3fad4a9d91d1a2963ee3fb1531250b70b927be00
gedion <117908662+Gedion-01@users.noreply.github.com>
2025-01-01 20:08:18 +0300

    added tanstack query

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,12 @@
 import "./assets/main.css";
 import router from "@/router";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 
 import { createApp } from "vue";
 import App from "./App.vue";
 
 const app = createApp(App);
 app.use(router);
+app.use(VueQueryPlugin);
 
 app.mount("#app");
