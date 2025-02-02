8bfb4637065673691fc244bb0fc53232972a2858
gedion <117908662+Gedion-01@users.noreply.github.com>
2025-02-02 15:03:59 +0300

    protected routes, added redirects and ui updates

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -3,6 +3,8 @@ import HomeView from "@/views/HomeView.vue";
 import SearchVue from "@/views/SearchView.vue";
 import HotelView from "@/views/HotelView.vue";
 import BookView from "@/views/BookView.vue";
+import AuthView from "@/views/AuthView.vue";
+import { useAuthStore } from "@/store/authStore";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -26,8 +28,25 @@ const router = createRouter({
       path: "/booking",
       name: "Book",
       component: BookView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/auth",
+      name: "Auth",
+      component: AuthView,
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    authStore.setRedirectUrl(to.fullPath);
+    next("/auth?tab=login");
+  } else {
+    next();
+  }
 });
 
 export default router;
